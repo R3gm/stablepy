1579c3e0ea3a95a2022b9c90129de556dbae7b8a
Roger Condori <114810545+R3gm@users.noreply.github.com>
2023-10-20 23:24:14 -0400

    Adetailer returns the image if something was not detected

--- stablepy/diffusers_vanilla/adetailer.py
+++ stablepy/diffusers_vanilla/adetailer.py
@@ -1,3 +1,5 @@
+# @title ad
+%%writefile adetailer.py
 # =====================================
 # Adetailer
 # =====================================
@@ -63,7 +65,11 @@ def ad_model_process(
         torch.cuda.empty_cache()
         gc.collect()
 
-        image_list_ad.append(result_ad[0][0])
+        try:
+            image_list_ad.append(result_ad[0][0])
+        except:
+            print("ADETAILER: No detections found")
+            image_list_ad.append(images_ad)
 
     torch.cuda.empty_cache()
     gc.collect()
